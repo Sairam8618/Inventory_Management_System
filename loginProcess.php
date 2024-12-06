55d69a1fe745ff2c4f0feb3a4dd0d1fee6f81fae
Sairam8618 <doddapanenisairam25@gmail.com>
2024-12-05 22:30:12 -0800

    Add files via upload

---
+++ loginProcess.php
@@ -0,0 +1,38 @@
+<?php include('inc/header.php');
+session_start();
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shoping_cart";
+
+// Create connection
+$conn = mysqli_connect($localhost, $username, $password , $dbname);
+ 
+if(isset($_POST['submit'])){
+     $email = mysqli_real_escape_string($conn, $_POST['email']);
+     $password = $_POST['password'];
+    
+     $sql = "SELECT * FROM users WHERE email='$email' ";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+ $dbStoredPASSWORD = $row['password'];
+
+if (password_verify ($password, $dbStoredPASSWORD)) {
+     $_SESSION['customer'] = $email;
+     $_SESSION['customerid'] = $row['id'];
+     header('location:index.php');
+  } else {
+    header('location:login.php?message=1');
+//    $message =  'incorrect Credentials';
+  }
+  
+
+
+   
+    
+}
+
+
+
+
+?>
